fa34ea1078e6fbc4162140f7ed41d6a9b6e29851
Kirill Ermakov <lvstrng161@gmail.com>
2024-02-01 18:48:25 +0400

    test_csv.py was updated

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -4,6 +4,7 @@ import os
 def test_csv(create_zip):
     csv_path = os.path.join(create_zip, 'users.csv')
     with open(csv_path) as file:
-        reader = csv.DictReader(file)
+        reader = csv.reader(file)
         for row in reader:
+            assert isinstance(row, list)
             print(row)
